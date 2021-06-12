d0509ab926768f1bea00f4c072888aa23e169f0c
Luiz Guilherme <79282765+quintasluiz@users.noreply.github.com>
2021-06-12 17:39:46 -0300

    lista de convidados

---
+++ ListaDeConvidados/ListaDeConvidados/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ListaDeConvidados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
