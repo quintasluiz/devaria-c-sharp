9c3b9194f6a4974f3071bb9b222ec390708b4d2a
Luiz Guilherme <79282765+quintasluiz@users.noreply.github.com>
2021-06-12 15:40:42 -0300

    meu primeiro commit c-sharp

---
+++ TesteVariavelConstante/TesteVariavelConstante/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace TesteVariavelConstante
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            const string constante = "uma constante que nao muda";
+
+            if(args.Length == 0)
+            {
+                Console.WriteLine("Favor informar um numero inteiro");
+                return;
+            }
+            int inteiro;
+            bool teste = int.TryParse(args[0], out inteiro);
+
+            if(teste == false)
+            {
+                Console.WriteLine("O argummento informadonão é um numero inteiro");
+                return;
+            }
+            Console.WriteLine(constante);
+            Console.WriteLine("O numero informado foi: " + inteiro);
+        }
+    }
+}
---
+++ olaMundo/olaMundo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace olaMundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
