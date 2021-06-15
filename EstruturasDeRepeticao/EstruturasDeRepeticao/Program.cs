e9cf109e4bbdbe350833945e55d67e8392073cac
Luiz Guilherme <79282765+quintasluiz@users.noreply.github.com>
2021-06-15 15:04:39 -0300

    estrutura de repetição

---
+++ EstruturasDeRepeticao/EstruturasDeRepeticao/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace EstruturasDeRepeticao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length == 0)
+            {
+                Console.WriteLine("Favor informar palavras no argumento");
+                return;
+            }
+          //for
+          for(var indice = 0; indice < args.Length; indice++)
+            {
+                Console.WriteLine($"Estrutura for, indice e valor: {indice},{args[indice]}");
+            }
+            //while
+            var argumentosLidos = 0;
+          while(argumentosLidos < args.Length)
+            {
+                Console.WriteLine($"Estrutura while, argumentosLidos e valor lido: {argumentosLidos}, {args[argumentosLidos]}");
+                argumentosLidos++;
+            }
+        }
+    }
+}
